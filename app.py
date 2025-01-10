3a6d3cfd8ce99ba9dba15bdde2cb496363886cf9
edoardo <105643778+Scapizza@users.noreply.github.com>
2025-01-10 12:39:06 +0000

    a

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html", titolo="Home")
 
-app.run()
+@app.route("/login")
+def login():
+    return render_template("login.html", titolo="login")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", titolo="register")
+
+
+
+app.run(debug=True)
